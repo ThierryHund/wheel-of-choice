08cf91d54c2669ceab9551f89dae3b07c5f81673
thierry hund <thierry@certus-technologies.com>
2019-05-15 16:07:34 +0200

    fix autofocus on config

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, ViewChildren, ElementRef, QueryList, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConfigService } from 'src/app/services/config.service';
 import { Router } from '@angular/router';
+import { MatInput } from '@angular/material';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.css']
 })
-export class ConfigComponent implements OnInit {
+export class ConfigComponent implements OnInit, AfterViewInit {
 
 
-  constructor(private configService: ConfigService,private fb: FormBuilder, public router: Router) { }
+  constructor(private configService: ConfigService,private fb: FormBuilder, public router: Router, private cd: ChangeDetectorRef) { }
+
+  @ViewChildren('items') items: QueryList<ElementRef>;
 
   form = new FormGroup({
     configItems: new FormArray(
@@ -23,16 +27,26 @@ export class ConfigComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.items.changes.subscribe(() => {
+      this.items.last.nativeElement.focus();
+    });
+  }
+
   get configItems() {
     return this.form.get('configItems') as FormArray;
   }
 
-  addConfigItems(){
+  addConfigItems() {
     this.configItems.push(this.fb.control('',  Validators.required));
+    this.cd.detectChanges();
+
   }
 
   deleteConfigItems(i: number) {
-    this.configItems.removeAt(i)
+    this.configItems.removeAt(i);
+    this.cd.detectChanges();
+
   }
 
   onSubmit() {
